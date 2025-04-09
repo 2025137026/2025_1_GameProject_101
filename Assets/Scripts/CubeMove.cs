6dcb996407cc77ae2e53c82fd07c38d9e145de43
2025137026 <appleapdm@shingu.ac.kr>
2025-04-09 16:02:12 +0900

    [feat]랜덤 블록 나옴

---
+++ Assets/Scripts/CubeMove.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMove : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+
+    //Update is called once per frame
+
+    private void Update()
+    {
+        transform.Translate(0,0, - moveSpeed * Time.deltaTime); //축 마이너스 방향으로 이동
+
+        if(transform.position.z < -20)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
